c6f69950a1615aebcbd9afe48a92e8173d5c2962
Fred <guru.three@gmail.com>
2023-07-09 01:24:45 +0100

    restored internal buffer, timer, mutex lock

--- MicroMatrix.cpp
+++ MicroMatrix.cpp
@@ -5,22 +5,17 @@
 #include "IS31FL3730/IS31FL3730.h"
 #include "MicroMatrix.h"
 
-#define M1 0
-#define M2 1
-
 IS31FL3730 m1;
 IS31FL3730 m2;
 IS31FL3730 m3;
 
 MicroMatrix::MicroMatrix() : GFXcanvas8(MATRIXWIDTH, MATRIXHEIGHT), _lastFrameDrawn(1), _atlevel(1)
 {
-  //_displayBuffer = (uint8_t*)malloc(MATRIXWIDTH * MATRIXHEIGHT * sizeof(uint8_t));
-//  memset(_displayBuffer, 0, MATRIXWIDTH * MATRIXHEIGHT * sizeof(uint8_t));
+  memset(_displayBuffer, 0, MATRIXWIDTH * MATRIXHEIGHT * sizeof(uint8_t));
 }
 
 MicroMatrix::~MicroMatrix()
 {
-//  free(_displayBuffer);
 }
 
 void MicroMatrix::begin(void (*write_fun)(uint8_t, uint8_t*, size_t))
@@ -30,64 +25,49 @@ void MicroMatrix::begin(void (*write_fun)(uint8_t, uint8_t*, size_t))
   m3.begin(0x63, write_fun);
 
   // set Lighting Effect Register to 10mA
-  m1.set_le(0xE);
-  m2.set_le(0xE);
-  m3.set_le(0xE);
-  //m1.set_bright(20);
+  m1.set_le(0xB); // 20 mA
+  m2.set_le(0xB);
+  m3.set_le(0xB);
+
+  m1.set_bright(20);
+  m2.set_bright(20);
+  m3.set_bright(20);
 
   m1.clear();
   m2.clear();
   m3.clear();
+
+  mutex_init(&mx1);
 }
 
+// ~50 us + the time the update takes
 void MicroMatrix::display()
 {
-printf("display\n");
+  // read off of display buffer as soon as possible to keep to timing for display
+  // so block until we can
+  mutex_enter_blocking(&mx1); 
   for (uint16_t y = 0; y < 7; y++) {
+    uint16_t w = y * WIDTH;
     for (uint16_t x = 0; x < 5; x++) {
-//      m1.set_pixel(M1, 4-x, 6-y, getRawPixel(x, y));
-//      m1.set_pixel(M2, 4-x, 6-y, getRawPixel(x+5, y));
-//      m2.set_pixel(M1, 4-x, 6-y, getRawPixel(x+10, y));
-//      m2.set_pixel(M2, 4-x, 6-y, getRawPixel(x+15, y));
-//      m3.set_pixel(M1, 4-x, 6-y, getRawPixel(x+20, y));
-//      m3.set_pixel(M2, 4-x, 6-y, getRawPixel(x+25, y));
-      
-//_displayBuffer[(whichRow * WIDTH) + (i * 8) + j]
-// [(x / 8) + y * ((WIDTH + 7) / 8)]
-//      m1.set_pixel(M1, 4-x, 6-y, _displayBuffer[x + y * WIDTH] > _atlevel);
-//      m1.set_pixel(M2, 4-x, 6-y, _displayBuffer[(x+5) + y * WIDTH] > _atlevel);
-//      m2.set_pixel(M1, 4-x, 6-y, _displayBuffer[(x+10) + y * WIDTH] > _atlevel);
-//      m2.set_pixel(M2, 4-x, 6-y, _displayBuffer[(x+15) + y * WIDTH] > _atlevel);
-//      m3.set_pixel(M1, 4-x, 6-y, _displayBuffer[(x+20) + y * WIDTH] > _atlevel);
-//      m3.set_pixel(M2, 4-x, 6-y, _displayBuffer[(x+25) + y * WIDTH] > _atlevel);
-      m1.set_pixel(M1, 4-x, y, getBuffer()[x + y * WIDTH] > _atlevel);
-      m1.set_pixel(M2, 4-x, y, getBuffer()[(x+5) + y * WIDTH] > _atlevel);
-      m2.set_pixel(M1, 4-x, y, getBuffer()[(x+10) + y * WIDTH] > _atlevel);
-      m2.set_pixel(M2, 4-x, y, getBuffer()[(x+15) + y * WIDTH] > _atlevel);
-      m3.set_pixel(M1, 4-x, y, getBuffer()[(x+20) + y * WIDTH] > _atlevel);
-      m3.set_pixel(M2, 4-x, y, getBuffer()[(x+25) + y * WIDTH] > _atlevel);
+      m1.set_pixel1(x, y, _displayBuffer[(x+25) + w] > _atlevel);
+      m1.set_pixel2(x, y, _displayBuffer[(x+20) + w] > _atlevel);
+      m2.set_pixel1(x, y, _displayBuffer[(x+15) + w] > _atlevel);
+      m2.set_pixel2(x, y, _displayBuffer[(x+10) + w] > _atlevel);
+      m3.set_pixel1(x, y, _displayBuffer[(x+5) + w] > _atlevel);
+      m3.set_pixel2(x, y, _displayBuffer[x + w] > _atlevel);
     }
   }
-//m1.set_pixel(M1, 0, 1, 1);
-
-/*char temp[100];
-  for (uint16_t y = 0; y < HEIGHT; y++) {
-    for (uint16_t x = 0; x < WIDTH; x++) {
-//        sprintf(temp, "%s%d", temp, getBuffer()[(x+20) + y * WIDTH]);
-        printf("full map %d, %d is \n", x, y);//, getBuffer()[x + y * WIDTH]);
-}
-//    printf("%s\n", temp);
-}*/
+  mutex_exit(&mx1);
 
   m1.update();
   m2.update();
   m3.update();
+
   _lastFrameDrawn = 1;
   
   _atlevel++;
   if (_atlevel == getBrightnessLevels())
     _atlevel = 0;
-printf("end display\n");
 }
 
 void MicroMatrix::clearDisplay()
@@ -114,24 +94,26 @@ uint8_t MicroMatrix::flip()
 
 void MicroMatrix::forceFlip()
 {
-//  noInterrupts();
-printf("forceflip\n");
+  bool locked = mutex_enter_timeout_ms(&mx1, 2); // request lock
+  if (locked) {
+    memcpy(_displayBuffer, getBuffer(), WIDTH * HEIGHT * sizeof(uint8_t));
+    _lastFrameDrawn = 0; // we now need to draw this frame
+    mutex_exit(&mx1); // unlock
+  }
+}
 
-//memcpy(_displayBuffer, getBuffer(), WIDTH * HEIGHT * sizeof(uint8_t));
+void MicroMatrix::enableTimer()
+{
+  repeating_timer_callback_t loopTask = [](repeating_timer_t *rt)->bool
+  {
+    MicroMatrix* instance = (MicroMatrix*)(rt->user_data);
+    instance->display();
+    return true;
+  };
+  add_repeating_timer_us(1050, loopTask, this, &timer);
+}
 
-//for (int i = 0; i < WIDTH * HEIGHT * sizeof(uint8_t); i++)
-//    _displayBuffer[i] =  getBuffer()[i];
-
-//_displayBuffer[1] = 100;
-
-//_displayBuffer[1] = getBuffer()[1];
-
-//printf("buffer2: %p\n", getBuffer());
-//   printf("_dB is %d\n", _displayBuffer[1]);
-//   printf("gB) is %d\n", getBuffer()[1]);
-
-//printf("val %d\n", getPixel(1, 1));
-
-  _lastFrameDrawn = 0; // we now need to draw this frame
-//  interrupts();
+void MicroMatrix::disableTimer()
+{
+  cancel_repeating_timer(&timer);
 }
--- MicroMatrix.h
+++ MicroMatrix.h
@@ -1,6 +1,10 @@
 #ifndef _MICROMATRIX_H
 #define _MICROMATRIX_H
 
+// GFXcanvas8 treated as 256 greyscale
+
+#include "pico/multicore.h"
+
 #include <Adafruit_GFX_Library/Adafruit_GFX.h>
 
 #define MATRIXWIDTH 30
@@ -8,13 +12,13 @@
 
 class MicroMatrix : public GFXcanvas8
 {
-
-private:
-//  uint8_t _displayBuffer[MATRIXWIDTH*MATRIXHEIGHT];
+ private:
+  uint8_t _displayBuffer[MATRIXWIDTH*MATRIXHEIGHT];
   uint8_t _lastFrameDrawn; // 1 when ready for next frame
   uint8_t _atlevel;
-
-public:
+  struct repeating_timer timer;
+  mutex_t mx1; // lock while copying buffers
+ public:
   MicroMatrix();
   virtual ~MicroMatrix(void); 
   
@@ -24,9 +28,10 @@ public:
   uint8_t wasLastFrameDrawn(void);
   uint8_t flip(void);
   void forceFlip(void);
+  void enableTimer(void);
+  void disableTimer(void);
 
-  uint8_t getBrightnessLevels() { return 10; };
-  
+  uint8_t getBrightnessLevels() { return 12; }; 
 };
 
 #endif
